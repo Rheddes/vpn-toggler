ed08ca21b1e0d80d7dce8cd50e65517840228338
Rens Heddes <rens@easy-lms.com>
2022-05-18 11:00:31 +0200

    Fix publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 glib-compile-schemas schemas/
-zip -q vpn-toggler.zip schemas/custom-vpn-toggler.gschema.xml schemas/gschemas.compiled metadata.json prefs.js convenience.js extension.js preferenceswidget.js LICENSE images/icon.png images/security-high-symbolic.svg images/security-medium-symbolic.svg images/security-low-symbolic.svg
-unzip -l vpn-toggler.zip
+zip -q vpn-toggler@rheddes.nl.zip schemas/vpn-toggler.gschema.xml schemas/gschemas.compiled metadata.json prefs.js prefs.ui convenience.js extension.js LICENSE images/icon.png images/security-high-symbolic.svg images/security-medium-symbolic.svg images/security-low-symbolic.svg
+unzip -l vpn-toggler@rheddes.nl.zip
+rm -rf vpn-toggler@rheddes.nl
