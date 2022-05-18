6dd7b374f86aa63d4f7447a46c5f008f66a2d889
Rens Heddes <rens@easy-lms.com>
2022-05-18 10:50:55 +0200

    Update image

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 glib-compile-schemas schemas/
-zip -q custom-vpn-toggler.zip schemas/custom-vpn-toggler.gschema.xml schemas/gschemas.compiled metadata.json prefs.js convenience.js extension.js preferenceswidget.js LICENSE images/icon.png images/security-high-symbolic.svg images/security-medium-symbolic.svg images/security-low-symbolic.svg
-unzip -l custom-vpn-toggler.zip
+zip -q vpn-toggler.zip schemas/custom-vpn-toggler.gschema.xml schemas/gschemas.compiled metadata.json prefs.js convenience.js extension.js preferenceswidget.js LICENSE images/icon.png images/security-high-symbolic.svg images/security-medium-symbolic.svg images/security-low-symbolic.svg
+unzip -l vpn-toggler.zip
