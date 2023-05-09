cb5344671ca336541e88a98cff624e9d2143054a
Rens Heddes <rens@easy-lms.com>
2023-05-09 16:24:30 +0200

    Fix schema and publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-glib-compile-schemas schemas/
-zip -q vpn-toggler@rheddes.nl.zip schemas/vpn-toggler.gschema.xml schemas/gschemas.compiled metadata.json prefs.js prefs.ui extension.js LICENSE images/icon.png images/security-high-symbolic.svg images/security-medium-symbolic.svg images/security-low-symbolic.svg "$(ls locale/**/*.mo)"
+zip -q vpn-toggler@rheddes.nl.zip schemas/org.gnome.shell.extensions.vpn-toggler.gschema.xml metadata.json prefs.js prefs.ui extension.js LICENSE images/icon.png images/security-high-symbolic.svg images/security-medium-symbolic.svg images/security-low-symbolic.svg "$(ls locale/**/*.mo)"
 unzip -l vpn-toggler@rheddes.nl.zip
 rm -rf vpn-toggler@rheddes.nl
